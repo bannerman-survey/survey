dc09397992d8431d3fdeffd336c75c830a4f4112
jjkusowski <jjkusowski@yahoo.com>
2018-04-03 12:36:45 -0700

    server done except convertkit integration

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -6,7 +6,49 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+let isServerOn = false;
+let httpServer = null;
+
 app.all('*', (request, response) => {
   console.log('hello world');
   return response.sendStatus(404);
 });
+
+const server = (module.exports = {});
+
+server.start = () => {
+  return new Promise((resolve, reject) => {
+    if(isServerOn) {
+      console.log('ERROR: Server is already on');
+      return reject(new Error('ERROR: Server is already on'));
+    }
+    if(!PORT) {
+      console.log('ERROR: Port variable is not configured');
+      return reject(new Error('ERROR: Port variable is not configured'));
+    }
+    httpServer = app.listen(PORT, () => {
+      isServerOn = true;
+      console.log(`Server is listening on port ${PORT}`);
+      return resolve();
+    });
+  });
+};
+
+server.stop = () => {
+  return new Promise((resolve, reject) => {
+    if (!isServerOn) {
+      console.log('ERROR: Server is already off');
+      return reject(new Error('ERROR: Server is already off'));
+    }
+    if (!httpServer) {
+      console.log('ERROR: server can\'t be shut down, server does not exist');
+      return reject(new Error('ERROR: server can\'t be shut down, server does not exist'));
+    }
+    httpServer.close(() => {
+      isServerOn = false;
+      httpServer = null;
+      console.log('Server is off');
+      return resolve();
+    });
+  });
+};
---
+++ backend/route/user-router.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const {Router} = require('express');
+const jsonParser = require('body-parser').json();
+const superagent = require('superagent');
+
+const userRouter = (module.exports  = new Router());
+
+userRouter.post('/signup', jsonParser, (request, response) => {
+  //TODO: more validation based on object
+  if(!request.body.email) {
+    return new Error(400, `ERROR: email is required`);
+  }
+  //TODO: Refactor post to Convert Kit
+  return superagent.post('convertkit.com/blah')
+    .attach(request.body)
+    .then( () => {
+      response.sendStatus(200);
+    });
+});
