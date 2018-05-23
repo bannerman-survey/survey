06a2d8d74fdd4fbc3ffa4951572c41e80ed76f36
jjkusowski <jjkusowski@yahoo.com>
2018-05-23 14:44:11 -0700

    second prompt appears

--- frontend/src/component/question/index.js
+++ frontend/src/component/question/index.js
@@ -10,6 +10,7 @@ class Question extends React.Component {
     super(props);
     this.state = {
       questionIndex: 0,
+      firstClicked: false,
     };
     let memberFunctions = Object.getOwnPropertyNames(Question.prototype);
     for (let functionName of memberFunctions) {
@@ -18,14 +19,29 @@ class Question extends React.Component {
       }
     }
   }
-
+  handleFirstClick(event) {
+    event.preventDefault();
+    this.setState({firstClicked: true});
+  
+  }
+  handleSecondClick(event) {
+    event.preventDefault();
+    console.log('before', this.state.questionIndex);
+    this.setState({ questionIndex: this.state.questionIndex + 1 });
+    console.log('after', this.state.questionIndex);
+  }
   render() {
+    let modifiers = (
+      <button onClick={this.handleSecondClick}> Thumbs Up</button>
+    );
+    let renderedModifiers = this.state.firstClicked === true ? modifiers : undefined;
     return <div className="question">
       <h1> {questions[this.state.questionIndex].question} </h1>
-      <button> {questions[this.state.questionIndex].data[0].description}</button>
+      <button onClick={this.handleFirstClick} > {questions[this.state.questionIndex].data[0].description}</button>
       <button> {questions[this.state.questionIndex].data[1].description}</button>
       <button> {questions[this.state.questionIndex].data[2].description}</button>
       <button> {questions[this.state.questionIndex].data[3].description}</button>
+      <div> {renderedModifiers}</div>
     </div>;
   }
 }
--- frontend/src/data/questions.js
+++ frontend/src/data/questions.js
@@ -27,6 +27,32 @@ const questions = [
       },
     ],
   },
+  {
+    sequence: 2,
+    question: 'This is question 2?',
+    data: [
+      {
+        responseKey: 1,
+        description: 'no tasks on my list',
+        value: 2,
+      },
+      {
+        responseKey: 2,
+        description: 'a few',
+        value: 5,
+      },
+      {
+        responseKey: 3,
+        description: 'many',
+        value: 8,
+      },
+      {
+        responseKey: 4,
+        description: 'tons',
+        value: 11,
+      },
+    ],
+  },
 ];
 
 export default questions;
