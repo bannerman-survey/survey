93ef0e43d657957a5cf48f67a81d930b8487e690
jjkusowski <jjkusowski@yahoo.com>
2018-05-23 14:02:44 -0700

    first question and button appearing

--- frontend/src/component/question/index.js
+++ frontend/src/component/question/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import questions from '../../data/questions';
+
 
 class Question extends React.Component {
-  render() {
-   
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionIndex: 0,
+    };
+    let memberFunctions = Object.getOwnPropertyNames(Question.prototype);
+    for (let functionName of memberFunctions) {
+      if (functionName.startsWith('handle')) {
+        this[functionName] = this[functionName].bind(this);
+      }
+    }
+  }
 
-    
-    return (
-      <div className="question">
-        <h1> Questions go here </h1>
-        <p>Questions</p>
-      </div>
-    );
+  render() {
+    return <div className="question">
+      <h1> {questions[this.state.questionIndex].question} </h1>
+      <button> {questions[this.state.questionIndex].data[0].description}</button>
+      <button> {questions[this.state.questionIndex].data[1].description}</button>
+      <button> {questions[this.state.questionIndex].data[2].description}</button>
+      <button> {questions[this.state.questionIndex].data[3].description}</button>
+    </div>;
   }
 }
 
--- frontend/src/data/questions.js
+++ frontend/src/data/questions.js
@@ -1,28 +1,32 @@
 'use strict';
 
-let questions = {
-  sequence: 1,
-  question: 'How many tasks are on your todo list?',
-  data: [
-    {
-      responseKey: 1,
-      description: 'no tasks on my list',
-      value: 2,
-    },
-    {
-      responseKey: 2,
-      description: 'a few',
-      value: 5,
-    },
-    {
-      responseKey: 3,
-      description: 'many',
-      value: 8,
-    },
-    {
-      responseKey: 4,
-      description: 'tons',
-      value: 11,
-    },
-  ],
-};
+const questions = [
+  {
+    sequence: 1,
+    question: 'How many tasks are on your todo list?',
+    data: [
+      {
+        responseKey: 1,
+        description: 'no tasks on my list',
+        value: 2,
+      },
+      {
+        responseKey: 2,
+        description: 'a few',
+        value: 5,
+      },
+      {
+        responseKey: 3,
+        description: 'many',
+        value: 8,
+      },
+      {
+        responseKey: 4,
+        description: 'tons',
+        value: 11,
+      },
+    ],
+  },
+];
+
+export default questions;
