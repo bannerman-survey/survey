02f73fd20adae3f8cde7f29bfcff5af1c0d010ac
jjkusowski <jjkusowski@yahoo.com>
2018-06-29 09:51:48 -0700

    question component fleshed out

--- frontend/src/data/questions.js
+++ frontend/src/data/questions.js
@@ -53,6 +53,32 @@ const questions = [
       },
     ],
   },
+  {
+    sequence: 3,
+    question: 'This is question 3?',
+    data: [
+      {
+        responseKey: 1,
+        description: 'no tasks on my list',
+        value: 2,
+      },
+      {
+        responseKey: 2,
+        description: 'a few',
+        value: 5,
+      },
+      {
+        responseKey: 3,
+        description: 'many',
+        value: 8,
+      },
+      {
+        responseKey: 4,
+        description: 'tons',
+        value: 11,
+      },
+    ],
+  },
 ];
 
 export default questions;
